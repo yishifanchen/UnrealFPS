8f817803a5e1fe887fa83dd44a6d6496faa8a811
xbb <1290212854@qq.com>
2018-12-24 10:39:06 +0800

    添加动画模型，制作状态机

--- Assets/UnrealFPS/Core/Source/Weapon/WeaponReloadSystem.cs
+++ Assets/UnrealFPS/Core/Source/Weapon/WeaponReloadSystem.cs
@@ -25,15 +25,79 @@ namespace UnrealFPS
         [SerializeField]private float emptyReloadTime;
         //Sequential
         [SerializeField]private float startTime;
-        [SerializeField] private float iterationTime;
+        [SerializeField] private float iterationTime;//重复反复
         private bool isReloading;
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.R)&&!ClipsIsEmpty)
             {
-
+                isReloading = true;
+                switch (reloadType)
+                {
+                    case ReloadType.Default:
+                        DefaultReload();
+                        break;
+                    case ReloadType.Sequential:
+                        SequentialReload();
+                        break;
+                }
             }
+        }
+        /// <summary>
+        /// 重新计算弹药数量
+        /// </summary>
+        public virtual void ReCalculateAmmo()
+        {
+            if (clipCount >= maxBulletCount)
+            {
+                clipCount -= (maxBulletCount - bulletCount);
+                bulletCount = maxBulletCount;
+            }
+            else if(clipCount<maxBulletCount)
+            {
+                bulletCount = clipCount + bulletCount;
+                clipCount = 0;
+            }
+        }
+        public virtual void DefaultReload()
+        {
+            if (!BulletsIsEmpty)
+                StartCoroutine(Reload(reloadTime));
+            else
+                StartCoroutine(Reload(emptyReloadTime));
+        }
+        public virtual void SequentialReload()
+        {
+            StartCoroutine(Reload(CalculateMaxTime(bulletCount, maxBulletCount, startTime, iterationTime)));
+        }
+        /// <summary>
+        /// calculate bullet and clip count after a specified time
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public IEnumerator Reload(float time)
+        {
+            yield return new WaitForSeconds(time);
+            ReCalculateAmmo();
+            isReloading = false;
+            yield break;
+        }
+        /// <summary>
+        /// Calculates the maximum time required for recharging
+        /// </summary>
+        /// <param name="bulletCount"></param>
+        /// <param name="maxBulletCount"></param>
+        /// <param name="startTime"></param>
+        /// <param name="interationTime"></param>
+        /// <returns></returns>
+        public virtual float CalculateMaxTime(float bulletCount,float maxBulletCount,float startTime,float iterationTime)
+        {
+            float totalTime;
+            float requiredBullet = maxBulletCount - bulletCount;
+            totalTime = iterationTime * requiredBullet;
+            totalTime += startTime;
+            return totalTime;
         }
         public int BulletCount
         {
@@ -44,6 +108,28 @@ namespace UnrealFPS
                     bulletCount = value;
                 else
                     bulletCount = maxBulletCount;
+            }
+        }
+        public int MaxClipCount
+        {
+            get { return maxClipCount; }
+            set { maxClipCount = value; }
+        }
+        /// <summary>
+        /// 弹夹是否为空
+        /// </summary>
+        public bool ClipsIsEmpty
+        {
+            get
+            {
+                return clipCount <= 0;
+            }
+        }
+        public bool BulletsIsEmpty
+        {
+            get
+            {
+                return bulletCount <= 0;
             }
         }
     }
